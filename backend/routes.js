b0c50bf6565a79a09eccf709983b3ba670b67079
yxz <yixuan0808@gmail.com>
2023-12-18 22:55:29 +0800

    Refractored code and fixed vulnerabilities

--- backend/routes.js
+++ backend/routes.js
@@ -3,13 +3,11 @@ const router = express.Router();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
-
 require("dotenv").config()
 const mongoClient = require("mongodb").MongoClient
 const { ObjectId } = require("mongodb")
 const connectionString = process.env.DB_STRING
 const jwtSecret = process.env.JWT_SECRET
-
 
 mongoClient.connect(connectionString)
     .then(client => {
@@ -18,17 +16,21 @@ mongoClient.connect(connectionString)
         const postCollection = db.collection("Post")
         const commentCollection = db.collection("Comment")
         const userCollection = db.collection("User")
+        
+        async function userExists(username) {
+            const users = await userCollection.find({username: username}).toArray()
+            return users.length !== 0
+        }
 
-
-
-
-        router.get("/auth/user/exists/:userId", async (req, res) => {
-            const users = await userCollection.find({username: req.params.userId}).toArray()
-            res.send(users.length !== 0)
+        router.get("/auth/user/exists/:username", async (req, res) => {
+            await userExists(req.params.username)
         })
 
         router.post("/auth/user/signup", async (req, res) => {
-            // assume at frontend user exist is checked
+            // make sure that the user does not exist
+            if (await userExists(req.body.username)) {
+                res.send(false)
+            }
             const saltRounds = 10
             req.body.password = await bcrypt.hash(req.body.password, saltRounds)
             userCollection.insertOne(req.body)
@@ -42,21 +44,19 @@ mongoClient.connect(connectionString)
         })
 
         router.post("/auth/user/signin", async (req, res) => {
-            // assume at frontend user exists
+            // make sure that the user exists
             const { username, password } = req.body;
-            const user = await userCollection.find({username: username}).toArray()
-
-            if (user.length === 0){
+            if (!await userExists(username)) {
                 res.send(false)
-            }else{
-                const passwordMatch = await bcrypt.compare(password, user[0].password);
-                if (passwordMatch){
-                    const token = jwt.sign({ userId: user[0]._id }, jwtSecret, { expiresIn: '1d' })
-                    res.send({token})
-                }else{
-                    res.send(false)
-                }
             }
+            const user = await userCollection.find({username: username}).toArray()
+            const passwordMatch = await bcrypt.compare(password, user[0].password);
+            if (!passwordMatch){
+                res.send(false)
+            }
+            const token = jwt.sign({ userId: user[0]._id }, jwtSecret, { expiresIn: '1d' })
+            res.send({token})
+            
         })
 
         router.post("/auth/user/details", async (req, res) => {
@@ -65,21 +65,20 @@ mongoClient.connect(connectionString)
             jwt.verify(token, jwtSecret, (err, decoded) => {
                 if (err){
                     res.send(false)
-                }else{
-                    res.send({ "userId": decoded.userId})
                 }
+                res.send({ "userId": decoded.userId})
             })
         })
 
+        // TODO: this endpoint exposes all user data including username and hashed password
+        // find where this is used and change the code
         router.get("/auth/user/getUser/:userId", async (req, res) => {
             const userId = req.params.userId
             const user = await userCollection.find({_id: new ObjectId(userId)}).toArray()
             res.send(user)
         })
 
-
-
-
+        // TODO: all posts are send to the frontend(user) consider only querying the top
         router.get("/posts", async (req, res) => {
             const postsList = await postCollection.find({}).toArray()
             res.send(postsList)
@@ -95,6 +94,7 @@ mongoClient.connect(connectionString)
             }
         })
 
+        // TODO: user-controlled userid, use token instead
         router.post("/posts/add", (req, res) => {
             req.body.userId = new ObjectId(req.body.userId)
             postCollection.insertOne(req.body)
@@ -108,12 +108,14 @@ mongoClient.connect(connectionString)
                 })
         });
 
+        // TODO: similar to getting posts, the client could be flooded, but should be less dire
         router.get("/posts/comments/:postId", async (req, res) => {
             const postId = req.params.postId
             const comments = await commentCollection.find({parentPost: true, parentId: new ObjectId(postId)}).toArray()
             res.send(comments)
         })
 
+        // TODO: same as posts, user-controlled userid
         router.post("/posts/comments/add", (req, res) => {
             req.body.parentId = new ObjectId(req.body.parentId)
             commentCollection.insertOne(req.body)
@@ -125,6 +127,7 @@ mongoClient.connect(connectionString)
                 })
         })
 
+        // TODO: add validation for deletions
         router.delete("/posts/del/:postId", async (req, res) => {
             const postId = req.params.postId
             const deletedPost = await postCollection.findOneAndDelete({_id: new ObjectId(postId)})
